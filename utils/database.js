3d28e411d263c930f4c480902c17a2a0fb8fa056
Carlos Eduardo <carloseduardovii@gmail.com>
2022-06-10 12:33:11 -0500

    commit config db

--- utils/database.js
+++ utils/database.js
@@ -2,15 +2,37 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './set.env' });
-
+const credentials = {
+  development: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB,
+    logging: false,
+  },
+  production: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
 // Create a connection to database
-const database = new Sequelize({
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER_NAME,
-  password: process.env.DB_PASS,
-  database: process.env.DB,
-  logging: false,
-});
+let config = {};
+
+if (process.env.NODE_ENV === 'development') config = credentials.development;
+else config = credentials.production;
+console.log(config);
+
+const database = new Sequelize(config);
 
 module.exports = { database };
